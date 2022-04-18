bb855358f665927820b47c10c7432687e745189e
문슬기 <tmfrlghtjr@gmail.com>
2022-04-18 14:19:26 +0900

    update youtube list

--- youtube-project/src/components/video_item/video_item.jsx
+++ youtube-project/src/components/video_item/video_item.jsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import styled from 'styled-components'
 
-const VideoItem = props => <h1>{props.video.snippet.title}</h1>;
+const Wrapper = styled.div `
+  width: 50%;
+  padding: 0.2em;
+`
+const Youtubebox = styled.li `
+  display: flex;
+  align-items: center;
+  border: 1px solid lightgray;
+  cursor: pointer;
+  transition: transform 250ms ease-in;
+  box-shadow: 3px 3px 5px 0 rgb(191, 191, 191, 0.53);
+  &:hover {
+    transform: scale(1.02)
+  }
 
+  .Thumbnail {
+    width: 40%;
+    height: 100%;
+  }
+`
+const Metadata = styled.div `
+  margin-left: 0.2em;
+  
+  .Title, 
+  .ChannelTitle {
+    margin: 0;
+    font-size: 0.8rem;
+  }
+
+  .ChannelTitle {
+    font-size: 0.6rem;
+  }
+`
+
+const VideoItem = ({ video : {snippet}} ) => (
+  <Wrapper>
+    <Youtubebox>
+      <img className='Thumbnail' src={snippet.thumbnails.medium.url} alt="video thumbnail" />
+      <Metadata>
+        <p className='Title'>{snippet.title}</p>
+        <p className='ChannelTitle'>{snippet.channelTitle}</p>
+      </Metadata>
+    </Youtubebox>
+  </Wrapper>
+);
 export default VideoItem;
--- youtube-project/src/components/video_list/video_list.jsx
+++ youtube-project/src/components/video_list/video_list.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styled from 'styled-components';
+
+const Wrapper = styled.div `
+`
+const Youtubelist = styled.ul `
+  display: flex;
+  flex-wrap: wrap;
+  padding-left: 0;
+  list-style: none;
+`
+
 
 const VideoList = props => (
-  <ul>
-    {props.videos.map(video => (
-      <VideoItem key={video.id} video={video} />
-    ))}
-  </ul>
+  <Wrapper>
+    <Youtubelist>
+      {props.videos.map(video => (
+        <VideoItem key={video.id} video={video} />
+      ))}
+    </Youtubelist>
+  </Wrapper>
 );
 
 export default VideoList;
